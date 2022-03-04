daaf6e27f371c271eda9e2d23dbb4b45236f4822
s669m <santhosh.parampottupadam@dkfz-heidelberg.de>
2022-03-04 15:37:48 +0100

    adapting fast api backend changes

--- services/base/backend-flask-tfda/docker/b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t registry.hzdr.de/santhosh.parampottupadam/tfdamvp1/kaapana-backend:0.0.1 .
-sudo docker push registry.hzdr.de/santhosh.parampottupadam/tfdamvp1/kaapana-backend:0.0.1
-microk8s kubectl delete pods -l app-name=backend -n base
