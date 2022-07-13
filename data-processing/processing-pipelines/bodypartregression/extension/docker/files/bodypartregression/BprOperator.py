be182f20ca8c571a0a9a5ac352945897eecf2578
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2022-07-13 10:28:35 +0200

    - Operator docs: BprOperator (Body Part Prediction)
    https://gitlab.hzdr.de/kaapana/kaapana/-/issues/123

--- data-processing/processing-pipelines/bodypartregression/extension/docker/files/bodypartregression/BprOperator.py
+++ data-processing/processing-pipelines/bodypartregression/extension/docker/files/bodypartregression/BprOperator.py
@@ -1,8 +1,28 @@
-from kaapana.kubetools.resources import Resources as PodResources
-from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, default_registry, default_platform_abbr, default_platform_version
 from datetime import timedelta
 
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, \
+    default_registry, default_platform_abbr, default_platform_version
+
+
 class BprOperator(KaapanaBaseOperator):
+    """
+    Regresses the body part a given slice is created from
+
+    The Body Part Regression (BPR) operator translates the anatomy in a
+    radiologic volume into a machine-interpretable form. Each axial slice maps
+    to a slice score. The slice scores monotonously increase with patient
+    height.
+
+    **Inputs:**
+
+    * Data in nifti format
+
+    **Outputs:**
+
+    * Regresses a slice score for each given sample
+
+    """
+
     execution_timeout = timedelta(minutes=10)
     task_dict = {}
 
@@ -14,12 +34,13 @@ class BprOperator(KaapanaBaseOperator):
                  execution_timeout=execution_timeout,
                  **kwargs
                  ):
-        envs = { "STRINGIFY_JSON": str(stringify_json)}
+        envs = {"STRINGIFY_JSON": str(stringify_json)}
         env_vars.update(envs)
 
         super().__init__(
             dag=dag,
-            image=f"{default_registry}/bodypartregression:{default_platform_abbr}_{default_platform_version}__v1.3",
+            image=f"{default_registry}/bodypartregression:"
+                  f"{default_platform_abbr}_{default_platform_version}__v1.3",
             name="bodypartregression",
             parallel_id=parallel_id,
             image_pull_secrets=["registry-secret"],
@@ -29,4 +50,4 @@ class BprOperator(KaapanaBaseOperator):
             gpu_mem_mb=5000,
             env_vars=env_vars,
             **kwargs
-        ) 
+        )
