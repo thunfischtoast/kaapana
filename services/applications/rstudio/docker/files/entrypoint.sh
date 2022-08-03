c06f9ec3afe45317ff2965559974b19ca5868809
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2022-08-03 14:12:02 +0200

    first version working

---
+++ services/applications/rstudio/docker/files/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# rstudio-server start
+# nginx
+# nginx -g 'daemon off;'
+
+
+echo "INIT USER: $USER"
+useradd $USER 
+echo "$USER:$PASSWORD" | chpasswd
+mkdir /home/$USER && chown -R $USER /home/$USER
+
+echo "Starting RStudio-Server..."
+/usr/lib/rstudio-server/bin/rserver --server-daemonize 0
